993f2146f6711258663d31620e90ebd69676c1a2
Antonio Montilla <system.montilla@gmail.com>
2024-03-05 14:03:24 +0800

    add auth JWT token

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -12,26 +14,39 @@ export const signup = async (req, res) => {
         if(user){
             return res.status(400).json({error: 'Username already exists'})
         }
+
         // hash password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
+        
         //randon avatar https://avatar-placeholder.iran.liara.run/
         const boyProfilePic =`https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic =`https://avatar.iran.liara.run/public/boy?username=${username}`
-
+        
         const newUser = new User({
             fullName,
             username,
-            password,
+            password:hashedPassword,
             gender,
             profilePic: gender === 'male' ? boyProfilePic : girlProfilePic
         })
 
-        await newUser.save();
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            profilePic: newUser.profilePic
-        });
+        if(newUser){
+            // Generate JWT token here 
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save(); 
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                profilePic: newUser.profilePic
+            });
+        }else{
+            res.status(400).json({
+                error: "Invalid user Data"
+            });
+        }
 
     } catch (error) {
         console.error("Error saving user:", error);
